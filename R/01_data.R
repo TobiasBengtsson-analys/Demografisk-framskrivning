5b8cbdc1d17e1f2ab4d9125af7f754c390dd7cf7
TobiasBengtsson-analys <tobbefirefire@gmail.com>
2026-04-27 14:46:53 +0200

    Update 01_data.R

--- R/01_data.R
+++ R/01_data.R
@@ -1,1 +1,68 @@
+library(pxweb)
 
+
+# Folkmängden efter ålder och kön. År 1968 - 2024
+
+befolkning <- pxweb_get(
+  url = "https://api.scb.se/OV0104/v1/doris/sv/ssd/START/BE/BE0101/BE0101A/BefolkningR1860N", 
+  query = list(
+    Kon = c("1", "2"), 
+    Alder = c("*"), 
+    ContentsCode = c("0000053A"), 
+    Tid = as.character(2000:2024)
+  )
+)
+
+befolkning_df = as.data.frame(befolkning, column.name.type = "text", 
+                              variable.value.type = "text")
+
+
+# Levande födda efter moderns ålder
+
+Födda <- pxweb_get(
+  url = "https://api.scb.se/OV0104/v1/doris/sv/ssd/START/BE/BE0101/BE0101H/FoddaK", 
+  query = list( 
+    AlderModer = c("*"),
+    Kon = c("*"), 
+    Region = c("00"),
+    ContentsCode = c("BE0101E2"), 
+    Tid = as.character(2000:2024))
+)
+
+Födda_df <- as.data.frame(Födda, column.name.type = "text", 
+                          variable.value.type = "text")
+
+
+# Döda efter ålder och kön
+
+Döda <- pxweb_get(
+  url = "https://api.scb.se/OV0104/v1/doris/sv/ssd/START/BE/BE0101/BE0101I/DodaHandelseK", 
+  query = list(
+    Kon = c("1", "2"), 
+    Alder = c("*"),
+    Region = c("00"), 
+    ContentsCode = c("BE0101D9"), 
+    Tid = as.character(1968:2024)
+  )
+)
+
+Döda_df <- as.data.frame(Döda, column.name.type = "text", 
+                         variable.value.type = "text")
+
+
+# In- och utvandring efter ålder och kön
+
+Migration <- pxweb_get(
+  url = "https://api.scb.se/OV0104/v1/doris/sv/ssd/START/BE/BE0101/BE0101J/ImmiEmiFlyttN", 
+  query = list(
+    Alder = c("*"),
+    Kon = c("1", "2"),
+    ContentsCode = c("*"),
+    UtInFlyttnLand = c("TOT"),
+    Fodelselandgrupp = c("SAMT2"),
+    Tid = as.character(2000:2024)
+  )
+)
+
+Migration_df <- as.data.frame(Migration, column.name.type = "text", 
+                              variable.value.type = "text")
